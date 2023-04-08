24e4350c886177c9ec820bddbad3c994155fef2c
Franco Passeggi <francopasseggi@gmail.com>
2023-04-08 16:18:51 -0300

    Fix navbar text when theme is dark

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,22 +3,27 @@ import { Link } from 'react-router-dom';
 import { ContextGlobal } from './utils/global.context';
 
 const Navbar = () => {
-  const { toggleTheme } = useContext(ContextGlobal);
+  const { state, toggleTheme } = useContext(ContextGlobal);
+  const { theme } = state;
+
+  const style = {
+    color: theme === 'light' ? 'black' : 'white'
+  }
 
   return (
-    <nav
-    >
+    <nav>
       <button
+        // className={`theme-button ${theme === 'light' ? 'light' : 'dark'}`}
         onClick={toggleTheme}
       >
         Change theme
       </button>
 
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/favs">Favorites</Link>
+      <Link style={style} to="/">Home</Link>
+      <Link style={style} to="/contact">Contact</Link>
+      <Link style={style} to="/favs">Favorites</Link>
 
-    </nav>
+    </nav >
   );
 };
 
